58141ad4ad41d65ca420cbba47243155c5dcb453
okeomasilachi onyedibia <110942635+okeomasilachi@users.noreply.github.com>
2024-07-28 00:18:10 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // backgroundImage: {
+      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      //   "gradient-conic":
+      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // },
+        maxWidth: {
+          'container-sm': '540px',
+          'container-md': '720px',
+          'container-lg': '960px',
+          'container-xl': '1140px',
+          'container-xxl': '1320px',
+        },
+        screens: {
+          'xxl': '1400px',
       },
     },
   },
